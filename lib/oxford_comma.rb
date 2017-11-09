84194c86e5968526a1c1cfca3dac049d1a76f750
TJ Carney <tjcarney@stny.rr.com>
2017-11-09 18:13:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+      array.join
+  elsif array.size == 2
+      array.join(" and ")
+  elsif array.size >= 3
+      [array[0...-1].join(", "), array.last].join(", and ")
+  end
 end
